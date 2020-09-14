27db390305a98470f04669a849f752173542b899
Gonzalo Rosales <gonzalorosales13@gmail.com>
2020-09-13 21:38:19 -0400

    added users/index route to list all registered users

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -56,13 +56,34 @@ class RegisterAPI(MethodView):
             }
             return make_response(jsonify(responseObject)), 202
 
+class UserIndexAPi(MethodView):
+    """
+    User Index Resource
+    """
+    def get(self):
+        """
+        returns list of registered users (emails only)
+        """
+        listOfUsers = []
+        
+        for user in User.query.all():
+            listOfUsers.append(user.email)
+
+        return ', '.join(listOfUsers)
 
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
+userindex_view = UserIndexAPi.as_view('userindex_api')
 
 # add Rules for API Endpoints
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
     methods=['POST', 'GET']
+)
+
+auth_blueprint.add_url_rule(
+    '/users/index',
+    view_func=userindex_view,
+    methods=['GET']
 )
